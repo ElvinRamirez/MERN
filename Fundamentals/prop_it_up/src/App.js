ce14eaee0a47abb25f33a049b78fd64fafabc4b4
Elvin Ramirez <102904220+ElvinRamirez@users.noreply.github.com>
2022-10-29 10:03:00 -0400

    Cleaned 'prop_it_up' + 'Putting it Together'

--- Fundamentals/prop_it_up/src/App.js
+++ Fundamentals/prop_it_up/src/App.js
@@ -5,7 +5,7 @@ import PersonCard from './components/PersonCard';
 function App() {
   return (
     <div className="App">
-      //
+      
       <PersonCard firstName={'Jane'} lastName={'Doe'} age={'45'} hairColor={'Black'}/>
 
       <PersonCard firstName={'Millard'} lastName={'Smith'} age={'88'} hairColor={'Brown'}/>
---
+++ Fundamentals/putting_it_together/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import PersonCard from './components/PersonCard';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <PersonCard firstName={'Jane'} lastName={'Doe'} age={45} hairColor={'Black'}/>
+
+      <PersonCard firstName={'Millard'} lastName={'Smith'} age={88} hairColor={'Brown'}/>
+
+      <PersonCard firstName={'Elliot'} lastName={'Alderson'} age={28} hairColor={'Black'}/>
+
+      <PersonCard firstName={'Tyrell'} lastName={'Wellick'} age={35} hairColor={'Blonde'}/>
+    </div>
+  );
+}
+
+export default App;
